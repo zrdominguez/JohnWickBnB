007232c4a9d1a1fbe8518b36d8215c516d67b51e
zrdominguez <zechariahd@gmail.com>
2024-07-14 15:57:59 -0500

    removed test route in routes/index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+
 
 module.exports = router;
