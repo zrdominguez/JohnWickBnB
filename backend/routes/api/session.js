9643bda163b12fe264196bee80261668ded6ba27
zrdominguez <zechariahd@gmail.com>
2024-07-19 14:07:47 -0500

    fixed api/session endpoint to return the firstName and lastName of a user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -47,8 +47,10 @@ router.post(
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
-      username: user.username,
+      username: user.username
     };
 
     await setTokenCookie(res, safeUser);
