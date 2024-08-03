bfed72b26b6b4a6c76768dc66de7d5de0d0cb7fc
zrdominguez <zechariahd@gmail.com>
2024-08-03 15:20:06 -0500

    added router for images

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const imagesRouter = require('./images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -21,6 +22,10 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
+
+router.use('/spot-images', imagesRouter);
+router.use('/review-images', imagesRouter);
+
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
