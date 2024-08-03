0779ee0f71f8effc04e4b096af491aba371dc698
zrdominguez <zechariahd@gmail.com>
2024-08-03 15:20:22 -0500

    added endpoint to delete spot images

---
+++ backend/routes/api/images.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { Spot, Image, Review, Booking, User,sequelize} = require('../../db/models');
+const { check } = require('express-validator');
+const { checkBookingConflict, checkOwnership, checkIfExists } = require('../../utils/helper');
+const { handleValidationErrors } = require('../../utils/validation');
+const {requireAuth} = require('../../utils/auth');
+const { includes } = require('lodash');
+
+const router = express.Router();
+
+//Delete a Spot
+router.delete('/:imageId',
+  requireAuth,
+  async (req, res, next) => {
+    const type = req.originalUrl.split('/')[2].includes('spot') ?
+    'spot' : 'review';
+    const {imageId} = req.params;
+
+    console.log(type)
+
+    const image = await Image.findOne({
+      where : {
+        id : imageId,
+        imageableType: type
+      },
+      attributes:["id", "imageableId", "imageableType"]
+    })
+
+    const notFoundError = checkIfExists(image, "Image");
+    if(notFoundError) return next(notFoundError);
+
+    let instance;
+
+    if(type ==='spot'){
+      instance = await Spot.findByPk(image.imageableId,{
+        attributes:["id", "ownerId"]
+      })
+    }
+    // else{
+    //   instance = await Review.findByPk(image.imageableId,{
+    //     attributes:["id", "userId"]
+    //   })
+    // }
+    const { id } = req.user
+    const authError = checkOwnership(instance, true, id);
+    if(authError) return next(authError);
+
+    await image.destroy();
+
+    res.json({message: "Successfully deleted"});
+  }
+)
+
+module.exports = router
