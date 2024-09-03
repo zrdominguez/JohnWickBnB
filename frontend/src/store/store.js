ec4466f2761c709cca3cc14cfc4277019e5459a5
zrdominguez <zechariahd@gmail.com>
2024-09-03 10:05:01 -0500

    added a login in page for user

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,11 +4,11 @@ import {
   applyMiddleware,
   compose
 } from 'redux';
-
+import sessionReducer from './session';
 import { thunk } from 'redux-thunk';
 
 const rootReducer = combineReducers({
-
+  session: sessionReducer
 });
 
 let enhancer;
