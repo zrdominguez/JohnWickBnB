64192b7596a0179b27641558d5431bf3c4b7ed5f
zrdominguez <zechariahd@gmail.com>
2024-09-04 11:01:42 -0500

    added profile dropdown functionality

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,8 +1,52 @@
-import { CgProfile } from "react-icons/cg";
-export const ProfileButton = () => {
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { FaUserCircle } from 'react-icons/fa';
+import * as sessionActions from '../../store/session';
+
+
+export function ProfileButton({ user }) {
+
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef()
+
+  const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+  };
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+    const closeMenu = (e) => {
+      if(ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
-    <div>
-      <CgProfile />
-    </div>
-  )
+    <>
+      <button onClick={toggleMenu}>
+        <FaUserCircle />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+         <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
 }
